eac4368033145fef149fb632d9b805bdf7cc1223
Robert Lowe <RobLowe1992@Yahoo.com>
2017-08-04 13:38:59 -0400

    Added Chrome redux extension

--- dev/js/index.js
+++ dev/js/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import allReducers from "./reducers";
 import App from "./containers/App.js"
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
